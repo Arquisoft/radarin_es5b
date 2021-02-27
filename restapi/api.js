e78f5be6f4905f52cd231c31a43710c85ac7f2ae
UO269984 <UO269984@uniovi.es>
2021-02-27 12:23:38 +0100

    Añadiendo pruebas de rest API

--- restapi/api.js
+++ restapi/api.js
@@ -4,26 +4,59 @@ const router = express.Router()
 
 // Get all users
 router.get("/users/list", async (req, res) => {
-    const users = await User.find({}).sort('-_id') //Inverse order
+	const users = await User.find({}).sort('-_id') //Inverse order
 	res.send(users)
 })
 
 //register a new user
 router.post("/users/add", async (req, res) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    //Check if the device is already in the db
-    let user = await User.findOne({ email: email })
-    if (user)
-        res.send({error:"Error: This user is already registered"})
-    else{
-        user = new User({
-            name: name,
-            email: email,
-        })
-        await user.save()
-        res.send(user)
-    }
+	let name = req.body.name;
+	let email = req.body.email;
+	//Check if the device is already in the db
+	let user = await User.findOne({ email: email })
+	if (user)
+		res.send({error:"Error: This user is already registered"})
+	else{
+		user = new User({
+			name: name,
+			email: email,
+		})
+		await user.save()
+		res.send(user)
+	}
 })
 
-module.exports = router
+function a(req, res) {
+	console.log("En prueba")
+	res.send("Holaaaaa que tal???")
+}
+router.get("/prueba1", a)
+
+function init(app) {
+	app.get("/getObject", (req, res) => {
+		obj = {"a": "primero", "b": "segundo"}
+		res.send(obj)
+	})
+	
+	app.get("/getXML", (req, res) => {
+		res.format({
+			"text/xml": function() {
+				res.send("<t1><a /></t1>")
+			}
+		})
+	})
+	
+	app.get("/getHTML", (req, res) => {
+		res.send("<html><head></head><body><h1>Título</h1><p>Párrafo 1</p></body></html>")
+	})
+	
+	app.post("/getObject", (req, res) => {
+		console.log(req.headers)
+		console.log(req.body)
+		res.send({"status": "recived"})
+	})
+}
+module.exports = {
+	router: router,
+	init: init
+}
--- restapi/server.js
+++ restapi/server.js
@@ -1,30 +1,35 @@
 const express = require("express")
-const promBundle = require("express-prom-bundle");
+//const promBundle = require("express-prom-bundle");
 const cors = require('cors');
-const mongoose = require("mongoose")
-const api = require("./api") 
+//const mongoose = require("mongoose")
+api = require("./api")
 
-function connect(){
-    //The MONGO_URI variable is the connection string to MongoDB Atlas (for production). This env variable is created in heroku.
-    mongo_uri = process.env.MONGO_URI || "mongodb://localhost:27017"
-    mongoose.connect(mongo_uri, { useNewUrlParser: true,useUnifiedTopology: true }).then(() => {
-        const app = express()
+function connect() {
+	const app = express()
 
-        //Monitoring middleware
-        const metricsMiddleware = promBundle({includeMethod: true});
-        app.use(metricsMiddleware);
-
-        app.use(cors());
-        app.options('*', cors());
-        app.use(express.json())
-        app.use("/api", api)
-
-
-        app.listen(process.env.PORT || 5000, () => {
-            console.log("Server has started! Using db in "+mongo_uri)
-        })
-    })
+	//Monitoring middleware
+	//const metricsMiddleware = promBundle({includeMethod: true});
+	//app.use(metricsMiddleware);
+	app.use(cors());
+	app.options('*', cors());
+	app.use(express.json())
+	app.use("/api", api.router)
+	
+	api.init(app)
+	
+	app.get("/update", (req, res) => {
+		delete require.cache['./api.js'];
+		api = require("./api")
+		api.init(app)
+		res.send("Updated");
+	})
+	
+	//router.get("/prueba1", a)
+	let addr = [5000, "127.0.0.1"]
+	app.listen(...addr, () => {
+		console.log("Server has started! port: " + addr[1] + ":" + addr[0]/*Using db in " + mongo_uri*/)
+	})
 }
 
 // Connect to MongoDB database, the wait is for giving time to mongodb to finish loading
-setTimeout(connect,5000)
+setTimeout(connect, 5000)
