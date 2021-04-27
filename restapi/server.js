85fcc66642351c9eb99ec6f866e54f5bf741ed3d
UO269984 <UO269984@uniovi.es>
2021-04-27 23:46:33 +0200

    Arreglado el acceso a la restapi en heroku
    Se ha quitado el servidor https de la restapi
    Las pruebas de la restapi ahora usan el servidor http

--- restapi/server.js
+++ restapi/server.js
@@ -11,9 +11,8 @@ const api = require("./api")
 const usersManager = require("./UsersManager").users
 
 class Server {
-	constructor(addrHttp, addrHttps) {
-		this.addrHttp = addrHttp
-		this.addrHttps = addrHttps
+	constructor(addr) {
+		this.addr = addr
 	}
 	
 	async start() {
@@ -27,41 +26,18 @@ class Server {
 		this.app.use("/coords", api.coordsRouter)
 		api.init(this.app)
 		
-		if (process.env.DEPLOY == null || process.env.DEPLOY == "false") {
-			var credentials = {
-				key: fs.readFileSync("passwords/key.pem", "utf-8"),
-				cert: fs.readFileSync("passwords/cert.pem", "utf-8"),
-				passphrase: "test123..."
-			}
-		}
-		else {
-			var credentials = {
-				key: process.env.HTTPS_KEY,
-				cert: process.env.HTTPS_CERT,
-				passphrase: process.env.HTTPS_PASS
-			}
-		}
 		this.server = http.createServer(this.app)
-		await this.server.listen(...this.addrHttp)
-		this.serverStarted(this.addrHttp)
-		
-		this.serverHttps = https.createServer(credentials, this.app)
-		await this.serverHttps.listen(...this.addrHttps)
-		this.serverStarted(this.addrHttps)
-	}
-	
-	serverStarted(addr) {
-		console.log("Server has started! port: " + addr[1] + ":" + addr[0])
+		await this.server.listen(...this.addr)
+		console.log("Server has started! port: " + this.addr[1] + ":" + this.addr[0])
 	}
 	
 	async close() {
 		await this.server.close()
-		await this.serverHttps.close()
 	}
 }
 
-var ip = process.env.DEPLOY == null ? "127.0.0.1" : null
-var server = new Server([5000, ip], [5001, ip])
+var port = process.env.PORT == null ? 5000 : process.env.PORT
+var server = new Server([port, null])
 
 if (require.main == module) {
 	server.start()
