a96b3977a0c991fc9358de4b8949262a581ad7fa
Immortal <ignacio-oviedo@hotmail.com>
2021-04-28 21:46:07 +0200

    Division lista de amigos en cercanos y lejanos

--- webapp/src/components/Principal.js
+++ webapp/src/components/Principal.js
@@ -3,33 +3,66 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import Mapa from "./Mapa";
 
+import restapi from "../api/api";
 import api from "../api/userDataManager";
 
 //export default  function Principal() {
 
-class Principal extends React.Component{
-    constructor(){
-        super();
-        api.connect();
+class Principal extends React.Component {
+  constructor() {
+    super();
+    api.connect();
+
+    this.state = {
+      amigos: { cercanos: [], lejanos: [] },
+    };
+
+    setTimeout(this.listarAmigos(), 2000);
+  }
+
+  async listarAmigos() {
+    var listAmigos = await (await restapi.getFriendsCoords()).json();
+    var result = { cercanos: [], lejanos: [] };
+
+    for (var f of listAmigos) {
+      if (f.inAdviseDist) result.cercanos.push(f);
+      else result.lejanos.push(f);
     }
 
-  
+    result.cercanos[0] = { id: "1" };
+    result.cercanos[1] = { id: "2" };
 
-    render(){
+    this.setState({
+      amigos: result,
+    });
+  }
 
-        
+  render() {
     return (
-        <LoggedIn>
-            
-            <Mapa />
-            <nav class="Menu">
-                <h5>Welcome back, <Value src="user.vcard_fn" />.</h5>
-                <p>Amigos:</p>
-                <List src="user.friends" />
-            </nav>
-        </LoggedIn>);
-    }
+      <LoggedIn>
+        <Mapa />
+        <nav class="Menu">
+          <h5>
+            Welcome back, <Value src="user.vcard_fn" />.
+          </h5>
+
+          <p>Amigos cercanos:</p>
+          <ul>
+            {this.state.amigos.cercanos.map((amigo) => {
+              return <li> {amigo.id} </li>;
+            })}
+          </ul>
+
+          <p>Amigos lejanos:</p>
+          <ul>
+            {this.state.amigos.lejanos.map((amigo) => {
+              return <li> {amigo.webId} </li>;
+            })}
+          </ul>
+        </nav>
+      </LoggedIn>
+    );
+  }
 }
 
 export default Principal;
-
